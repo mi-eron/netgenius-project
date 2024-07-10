6776a12f5a4f02e2d3f25ffaa6afe1265d4ace25
Mieron <mieron.hailemelekot@gmail.com>
2024-07-10 17:32:13 -0400

    Navbar - component created

---
+++ src/components/ui/navbar/navbar.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import { useState, CSSProperties } from "react";
+import "./_navbar.scss";
+
+import { CgClose } from "react-icons/cg";
+import { RiMenu3Fill } from "react-icons/ri";
+
+interface CustomCSSProperties extends CSSProperties {
+    "--_i": number;
+}
+
+const navLinks = [
+    {
+        id: 1,
+        label: "Home",
+        path: "/",
+    },
+    {
+        id: 2,
+        label: "Services",
+        path: "/services",
+    },
+    {
+        id: 3,
+        label: "Portfolio",
+        path: "/ortfolio",
+    },
+    {
+        id: 4,
+        label: "Contact",
+        path: "/contact",
+    },
+];
+
+export const Navbar = () => {
+    const [isExtended, setExtended] = useState(false);
+
+    const toggleExtension = () => {
+        setExtended((prev) => !prev);
+    };
+
+    return (
+        <section className={`navbar ${isExtended && "navbar--bg"}`}>
+            <div className="nav_buttons">
+                <h3 className="logo h_2">NetGenius</h3>
+
+                <button className="nav_button" onClick={toggleExtension}>
+                    {isExtended ? (
+                        <CgClose className="h_2" />
+                    ) : (
+                        <RiMenu3Fill className="h_2" />
+                    )}
+                </button>
+            </div>
+
+            <ul className={`nav_links ${!isExtended && "nav_links--hidden"}`}>
+                {navLinks.map(({ id, label }) => (
+                    <li
+                        key={id}
+                        className="nav_item h_4"
+                        style={{ "--_i": id } as CustomCSSProperties}
+                    >
+                        {label}
+                    </li>
+                ))}
+            </ul>
+        </section>
+    );
+};
